56ea58f5f95f2cc2f79a684e43ec94a3f4a916c2
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2019-10-02 17:58:01 +0100

    added license

--- receive-exchange-flows-asyncio.py
+++ receive-exchange-flows-asyncio.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
-# WS client example to connect to TokenAnalyst websocket API to subscribe to exchange flows
+# Copyright 2019 TokenAnalyst  
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# WS client Python 3.x example to connect to TokenAnalyst websocket API to subscribe to exchange flows
 
 import json
 import asyncio
@@ -11,6 +25,7 @@ import sys
 
 logging.basicConfig(level=logging.INFO)
 
+# make sure to export your API key in the environment variables, using $ export API_KEY='...'
 API_KEY = os.environ['API_KEY']
 
 async def interpret(response, id, on_data, on_heartbeat, on_subscribed):
--- speedtest.py
+++ speedtest.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+# Copyright 2019 TokenAnalyst  
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# WS client Python 3.x to benchmark block propagation latency of TokenAnalyst to Blockchain.com 
+
 import json
 import asyncio
 import logging
@@ -13,8 +29,6 @@ import time
 logging.basicConfig(level=logging.INFO)
 
 API_KEY = os.environ['API_KEY']
-
-
 
 # Blockchain.com Protocol
 
